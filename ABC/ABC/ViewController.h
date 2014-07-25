dfa9483ce9bfc507d08998d1d8e55bdb2844d5d4
Kamlesh <kamlesh.jaiswal@kelltontech.com>
2014-07-25 15:38:39 +0530

    Add Project
    Add Project

---
+++ ABC/ABC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ABC
+//
+//  Created by user01 on 7/25/14.
+//  Copyright (c) 2014 kellton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ABC/ABC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ABC
+//
+//  Created by user01 on 7/25/14.
+//  Copyright (c) 2014 kellton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
